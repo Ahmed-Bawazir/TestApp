ab06b1f6f213c35356b13489daeed86555373d48
4HM3D <4HM3D7759988@gmail.com>
2023-12-29 13:24:08 +0300

    ffdf

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,29 @@
-//installing and add files to cache 
+//installing and add files to cache
+let nameOfCache = "v1";
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(nameOfCache);
+  await cache.addAll(resources);
+};
+let asset = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/js.js",
+  "/manifest.json",
+  "/date.json",
+  "/icon192.png",
+  "/icon512.png",
+];
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(nameOfCache).then(function (cache) {
+      for (let ass of asset) {
+        cache.add(new Request(ass));
+      }
+    })
+  );
+});
+/* //installing and add files to cache 
 let nameOfCache="v1";
 const addResourcesToCache = async (resources) => {
   const cache = await caches.open(nameOfCache);
@@ -19,7 +44,7 @@ self.addEventListener("install", (event) => {
      
     ]),
   );
-});
+}); */
 //fetch files from cache
 const putInCache = async (request, response) => {
   const cache = await caches.open(nameOfCache);
@@ -61,7 +86,7 @@ self.addEventListener("fetch", (event) => {
     cacheFirst({
       request: event.request,
       fallbackUrl: "/icon512.png",
-    }),
+    })
   );
 });
 //activate

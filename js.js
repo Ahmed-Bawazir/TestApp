5c308579d09b4f3e2919171bdace39f8bcff52fd
4HM3D <4HM3D7759988@gmail.com>
2024-01-03 18:31:48 +0300

    testing true3

--- js.js
+++ js.js
@@ -173,7 +173,7 @@ pro.then((e) => {
   content.className = "container";
   content.innerHTML = `
   <div class="container">
-    <h2>مواقيت الصلوات  ||Test"Good5"| مسجد الرحمة
+    <h2>مواقيت الصلوات  ||Test"Good10"| مسجد الرحمة
       <br/>
       <span>بمنطقة النقعة</span></h2>
    

e1e0f90cd01d0a00ca4b6618affe2d7fca017d7a
Ahmed-Bawazir <151995076+Ahmed-Bawazir@users.noreply.github.com>
2024-01-03 13:43:50 +0300

    Update service-worker.jsnew

--- service-worker.js
+++ service-worker.js
@@ -59,7 +59,7 @@ const cacheFirst = async ({ request, fallbackUrl }) => {
     });
   }
 };
-
+/*
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     cacheFirst({
@@ -67,7 +67,19 @@ self.addEventListener("fetch", (event) => {
       fallbackUrl: "/icon512.png",
     })
   );
+});*/
+//network first
+self.addEventListener('fetch', event => {
+ event.respondWith(
+   fetch(event.request).then(response => {
+     cache.put(event.request, response.clone());
+     return response;
+   }).catch(_ => {
+     return caches.match(event.request);
+   });
+ );
 });
+
 //activate
 
 const deleteCache = async (key) => {

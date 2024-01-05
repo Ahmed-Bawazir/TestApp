01880f0373c52648bec3fef184139edb3692624b
4HM3D <4HM3D7759988@gmail.com>
2024-01-05 11:13:31 +0300

    testing updateing-3

--- js.js
+++ js.js
@@ -27,12 +27,14 @@ async function updateNow() {
   const deel = (await caches.open("v1")).delete("/TestApp/date.json");
   console.log(deel);
   const aadd = (await caches.open("v1")).add("/TestApp/date.json");
+  const aadd1 = (await caches.open("v1")).add("./date1.json");
   console.log(aadd);
+  console.log(aadd1);
   //console.log(keyList);
   /* const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
   await Promise.all(cachesToDelete.map(deleteCache)); */
 
-  location.reload()
+  //location.reload()
   
  }
  else{
